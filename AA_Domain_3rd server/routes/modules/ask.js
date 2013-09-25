b9ff560a20f6881446025506893a27c6bcaf1e28
leonlee989 <lee_quee_leong82@hotmail.com>
2013-09-25 10:44:47 +0800

    Server 3 uploaded

---
+++ AA_Domain_3rd server/routes/modules/ask.js
@@ -0,0 +1,39 @@
+function Ask(stock, price, userId) {
+	this.stock = stock;
+	this.price = price;
+	this.userId = userId;
+	this.date = new Date();
+}
+
+Ask.prototype.getStock = function() {
+	return this.stock;
+}
+
+Ask.prototype.getPrice = function() {
+	return this.price;
+}
+
+Ask.prototype.getUserId = function() {
+	return this.userId;
+}
+
+Ask.prototype.getDate = function() {
+	return this.date;
+}
+
+Ask.prototype.getDateString = function() {
+	var dateString = [[AddZero(this.date.getDate()), AddZero(this.date.getMonth() + 1), this.date.getFullYear()].join("/"), [AddZero(this.date.getHours()), AddZero(this.date.getMinutes())].join(":"), this.date.getHours() >= 12 ? "PM" : "AM"].join(" ");
+	return dateString;
+}
+
+Ask.prototype.toString = function() {
+	var dateString = [[AddZero(this.date.getDate()), AddZero(this.date.getMonth() + 1), this.date.getFullYear()].join("/"), [AddZero(this.date.getHours()), AddZero(this.date.getMinutes())].join(":"), this.date.getHours() >= 12 ? "PM" : "AM"].join(" ");
+	return "stock: " + this.stock + ", price: " + this.price + ", userId: " + this.userId + ", date: " + dateString;
+}
+
+//Pad given value to the left with "0"
+function AddZero(num) {
+    return (num >= 0 && num < 10) ? "0" + num : num + "";
+}
+
+module.exports.Ask = Ask;
